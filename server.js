f2bfd14bb7e19a4925a561c9d03f304c1757c400
chinna0107 <kancharlahemanth89@gmail.com>
2026-06-23 12:39:13 +0530

    Production 2.23

--- server.js
+++ server.js
@@ -20,7 +20,10 @@ app.use(cors({
     } else {
       callback(new Error('Not allowed by CORS'));
     }
-  }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
 }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
