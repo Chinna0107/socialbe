e4d0dfbf3ce5912fe13a86cb45c70bc2234022c1
chinna0107 <kancharlahemanth89@gmail.com>
2026-06-23 12:41:32 +0530

    Production 2.23

--- server.js
+++ server.js
@@ -12,6 +12,20 @@ const allowedOrigins = [
   'https://www.socialvoicenews.com',
 ];
 
+// Handle preflight OPTIONS requests explicitly for Vercel
+app.options('*', cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+}));
+
 app.use(cors({
   origin: function (origin, callback) {
     // allow requests with no origin (like mobile apps or curl requests)
